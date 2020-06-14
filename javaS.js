f2ca0b640dd8b5b9773961b68bbf881511f6de11
tatalb <55159053+tatalb@users.noreply.github.com>
2020-06-14 17:36:26 -0300

    Add files via upload

---
+++ javaS.js
@@ -0,0 +1,22 @@
+function bordaAlterada(imagen) {
+    imagen.style.height = "250px"
+    imagen.style.width = "250px"
+    imagen.style.border = "4px #0066aa solid "
+}
+function bordaNormal(imagen) {
+    imagen.style.height = "150px"
+    imagen.style.width = "150px"
+    imagen.style.border = ""
+}
+function ajudeBlumenau() {
+    alert("Organização Guaxinim Melhor Que Capivara LTDA.\nCNPJ: 67.553.331/0001-20\nBanco: Banco do Brasil \nAgência: 3432-0\nC/C: 21934-5");
+}
+function ajudeMassaranduba() {
+    alert("Organização Guaxaranduba LTDA.\nCNPJ: 41.064.624/0001-50\nBanco: Caixa Econômica \nAgência: 0135-1\nC/C: 2526-7");
+}
+function ajudeRioDosCedros() {
+    alert("ONG Mattedixinim LTDA.\nCNPJ: 01.991.927/0001-01\nBanco: Viacredi \nAgência: 0101\nC/C: 987.223-55");
+}
+function ajudePomerode() {
+    alert("Waschbär Niedlicher Allesfresser LTDA.\nCNPJ: 91.532.874/0001-45\nBanco: Banco do Brasil \nAgência: 5548-7\nC/C: 68598-7");
+}
